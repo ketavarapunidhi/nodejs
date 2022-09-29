e0743c3a7d1fa85cdd11cdbee624c66c4501835b
ketavarapunidhi <107557355+ketavarapunidhi@users.noreply.github.com>
2022-09-28 22:02:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const http = require('http');
-const host = '0.0.0.0';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Node.js!');
-}); 
-server.listen(port, host, () => {
-   console.log('Web server running at http://%s:%s',host,port );
-});
+const printing = () => {
+  console.log("Hello Node.js!");
+};
+printing();
